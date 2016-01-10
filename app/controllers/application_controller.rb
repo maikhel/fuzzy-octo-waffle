387fe853890731255a63abd2080f437c6df21588
maikhel <mlecicki@gmail.com>
2016-01-10 21:14:45 +0100

    i18n almost works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,32 +1,34 @@
 class ApplicationController < ActionController::Base
+
+  before_action :set_locale
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :set_locale
 
-  def set_user_locale
-    session[:locale] = params.fetch(:locale) if params[:locale]
-  end
+  private
+    def set_user_locale
+      session[:locale] = params.fetch(:locale) if params[:locale]
+    end
 
-  def set_session_locale
-    I18n.locale = session[:locale] if session[:locale]
-  end
+    def set_session_locale
+      I18n.locale = session[:locale] if session[:locale]
+    end
 
-  def after_sign_out_path_for(resource)
-    ENV['LOGOUT_URL']
-  end
+    def after_sign_out_path_for(resource)
+      ENV['LOGOUT_URL']
+    end
 
-  # def after_sign_in_path_for(resource)
-  #   if resource && resource.admin?
-  #     rails_admin.dashboard_path
-  #   elsif resource && resource.is_not_activated_customer?
-  #     backend.locked_path
-  #   else
-  #     backend.companies_path
-  #   end
-  # end
+    # def after_sign_in_path_for(resource)
+    #   if resource && resource.admin?
+    #     rails_admin.dashboard_path
+    #   elsif resource && resource.is_not_activated_customer?
+    #     backend.locked_path
+    #   else
+    #     backend.companies_path
+    #   end
+    # end
 
-  def set_locale(locale)
-    I18n.locale = locale
-  end
+    def set_locale
+      I18n.locale = params[:locale] || I18n.default_locale
+    end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,7 @@
 class WelcomeController < ActionController::Base
   before_action :authenticate_user!
   layout 'application'
+  
   def index
     @users = User.all
   end
