f9b71d8179c25d272af684a86aa38cc579e0620c
Michał Łęcicki <mlecicki@gmail.com>
2016-05-11 23:35:44 +0200

    Add RegistrationsController create and update actions

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,6 @@
 class RegistrationsController < ApplicationController
 
-  before_action :set_registration, only: [:show, :edit]
+  before_action :set_registration, only: [:show, :edit, :update]
 
   def index
     @registrations = Registration.all
@@ -14,6 +14,31 @@ class RegistrationsController < ApplicationController
   def edit
   end
 
+  def create
+    @registration = Registration.new(registration_params)
+
+    respond_to do |format|
+      if @registration.save
+        format.html { redirect_to registrations_path }
+      else
+        format.html { render :new }
+        format.json { render json: @registration.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @registration.update(registration_params)
+        format.html { redirect_to registrations_path }
+        # format.json { render :show, status: :ok, location: @registration }
+      else
+        format.html { render :edit }
+        format.json { render json: @registration.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
 
   private
     # Use callbacks to share common setup or constraints between actions.
@@ -22,7 +47,7 @@ class RegistrationsController < ApplicationController
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def user_params
+    def registration_params
       params.require(:registration).permit(:start_date, :end_date, :field_of_study_id)
     end
 
--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -2,13 +2,17 @@ require 'spec_helper'
 
 RSpec.describe RegistrationsController, type: :controller do
 
+  let(:field_of_study) { create(:field_of_study) }
+  let(:valid_attributes) { attributes_for(:registration, field_of_study_id: field_of_study.id) }
+  let(:invalid_attributes) { attributes_for(:registration, start_date: nil) }
+
   describe "GET index" do
     it "has a 200 status code" do
       get :index
       expect(response.status).to eq(200)
     end
 
-    it "assigns @registrations and @course_groups" do
+    it "assigns @registrations" do
       create_list(:registration, 3)
       get :index
       expect(assigns(:registrations)).to eq(Registration.all)
@@ -55,6 +59,87 @@ RSpec.describe RegistrationsController, type: :controller do
     it "renders the new template" do
       get :edit, id: registration.id
       expect(response).to render_template('edit')
+    end
+  end
+
+  describe "POST #create" do
+
+    context "with valid params" do
+      it "creates a new Registration" do
+        expect {
+          post :create, registration: valid_attributes
+        }.to change(Registration, :count).by(1)
+      end
+
+      it "assigns a newly created registration as @registration" do
+        post :create, registration: valid_attributes
+        expect(assigns(:registration)).to be_a(Registration)
+        expect(assigns(:registration)).to be_persisted
+      end
+
+      it "redirects to the created registration" do
+        post :create, registration: valid_attributes
+        group = Registration.last
+        expect(response).to redirect_to registrations_path
+      end
+    end
+
+    context "with invalid params" do
+      it "assigns a newly created but unsaved registration as @registration" do
+        post :create, registration: invalid_attributes
+        expect(assigns(:registration)).to be_a_new(Registration)
+      end
+
+      it "re-renders the 'new' template" do
+        post :create, registration: invalid_attributes
+        expect(response).to render_template("new")
+      end
+    end
+  end
+
+  describe "PUT #update" do
+
+    it "assigns @registration " do
+      registration = create(:registration)
+      put :update, {
+        id: registration.id,
+        registration: valid_attributes
+      }
+      expect(assigns(:registration)).to eq(registration)
+    end
+
+    context "with valid params" do
+
+      it "updates the requested registration" do
+        registration = create(:registration)
+        new_field = create(:field_of_study)
+        put :update, {
+          id: registration.id,
+          registration: attributes_for(:registration, field_of_study_id: new_field.id)
+        }
+        registration.reload
+        expect(registration.field_of_study).to eq new_field
+      end
+
+      it "redirects to the registrations path" do
+        registration = create(:registration)
+        put :update, {
+          id: registration.id,
+          registration: valid_attributes
+        }
+        expect(response).to redirect_to registrations_path
+      end
+    end
+
+    context "with invalid params" do
+      it "re-renders the 'edit' template" do
+        registration = create(:registration)
+        put :update, {
+          id: registration.id,
+          registration: invalid_attributes
+        }
+        expect(response).to render_template("edit")
+      end
     end
   end
 
