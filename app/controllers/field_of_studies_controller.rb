4bdbcfe82289fdb3256b0bbd6c1f3a1f82400d67
Michał Łęcicki <mlecicki@gmail.com>
2016-05-27 21:48:43 +0200

    Add FieldOfStudyPolicy

--- app/controllers/field_of_studies_controller.rb
+++ app/controllers/field_of_studies_controller.rb
@@ -8,20 +8,24 @@ class FieldOfStudiesController < ApplicationController
 
   # GET /field_of_studies/1
   def show
+    authorize @field_of_study
   end
 
   # GET /field_of_studies/new
   def new
     @field_of_study = FieldOfStudy.new
+    authorize @field_of_study
   end
 
   # GET /field_of_studies/1/edit
   def edit
+    authorize @field_of_study
   end
 
   # POST /field_of_studies
   def create
     @field_of_study = FieldOfStudy.new(field_of_study_params)
+    authorize @field_of_study
 
     respond_to do |format|
       if @field_of_study.save
@@ -35,6 +39,8 @@ class FieldOfStudiesController < ApplicationController
 
   # PATCH/PUT /field_of_studies/1
   def update
+    authorize @field_of_study
+
     respond_to do |format|
       if @field_of_study.update(field_of_study_params)
         format.html { redirect_to field_of_studies_path }
@@ -47,6 +53,8 @@ class FieldOfStudiesController < ApplicationController
 
   # DELETE /field_of_studies/1
   def destroy
+    authorize @field_of_study
+
     @field_of_study.destroy
     respond_to do |format|
       format.html { redirect_to field_of_studies_path }
--- app/policies/field_of_study_policy.rb
+++ app/policies/field_of_study_policy.rb
@@ -1,17 +1,23 @@
 class FieldOfStudyPolicy < ApplicationPolicy
-  class Scope < Scope
-    def resolve
-      if user.admin? || user.staff?
-        scope.all
-      else
-        if user.partner and (user.partner != Partner.default)
-          user_companies_scope = user.companies.count  > 0 ? "companies.id IN (#{user.companies.map(&:id).join(",")}) OR" : ""
-          scope.where("#{user_companies_scope} partner_id = #{user.partner_id}")
-        else
-          scope.where(id: user.companies)
-        end
-      end
-    end
+
+  def new?
+    administrable?
+  end
+
+  def create?
+    administrable?
+  end
+
+  def edit?
+    administrable?
+  end
+
+  def update?
+    administrable?
+  end
+
+  def destroy?
+    administrable?
   end
 
 end
